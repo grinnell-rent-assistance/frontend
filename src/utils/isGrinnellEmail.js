f4d04e48f22a2cc7f88db1a1fd7f201d658207bf
Jonah Shin <shinjona@grinnell.edu>
2020-07-29 10:31:47 -0700

    Set min and max length for Grinnell username in isGrinnellEmail

--- src/utils/isGrinnellEmail.js
+++ src/utils/isGrinnellEmail.js
@@ -1,4 +1,8 @@
 export default function isGrinnellEmail(email) {
+  const user = email.trim().toLowerCase().substring(0, email.indexOf('@'));
   const re = /\S+@grinnell.edu$/;
-  return re.test(email.trim().toLowerCase());
+  return (
+    (re.test(email.trim().toLowerCase()))
+    && 
+    (user.length <= 8 && user.length >= 4));
 }
